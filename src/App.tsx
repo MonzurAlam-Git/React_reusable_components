5e391679515affe4eb1ddcb264b0416a25eefbf4
MonzurAlam <you@example.com>
2024-03-16 21:31:38 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import MainLayout from "./components/Layout/mainLayout";
+
+function App() {
+  return <MainLayout></MainLayout>;
+}
+
+export default App;
---
+++ src/components/Layout/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
